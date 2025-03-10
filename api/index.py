f8003a78a02478ac2fb65fe648183f493fb91390
Rodri <nunes90rodrigo@gmail.com>
2025-03-10 18:58:35 +0000

    update 1

--- api/index.py
+++ api/index.py
@@ -1,7 +1,21 @@
 from flask import Flask
+import psycopg2
 
 app = Flask(__name__)
 
+def db_connection():
+    try:
+        conn = psycopg2.connect(
+            user="postgres",
+            password="postgres",
+            host="localhost",
+            port="5432",
+            database="bdii"
+        )
+        return conn
+    except Exception as e:
+        print(e)
+        return None
 @app.route('/')
 def home():
     return 'Hello, World!- BD2-10/03/2025'
@@ -9,3 +23,12 @@ def home():
 @app.route('/about')
 def about():
     return 'About'
+
+@app.rout('/emp')
+def emp():
+    conn = db_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM emp;")
+
+    print(cur.fetchall())
+
